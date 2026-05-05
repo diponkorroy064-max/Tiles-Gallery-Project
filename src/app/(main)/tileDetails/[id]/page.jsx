b3d8e2ba19961fc22d70256b70304f139983c2b7
diponkorroy064-max <diponkorroy064@gmail.com>
2026-05-05 23:28:31 +0600

    add tileDetails page design and use params and find the object my using js find method

--- src/app/(main)/tileDetails/[id]/page.jsx
+++ src/app/(main)/tileDetails/[id]/page.jsx
@@ -1,21 +1,33 @@
+import { allTilesData } from '@/lib/fetch_data';
 import React from 'react';
 
 const TileDetailsPage = async({ params }) => {
     const { id } = await params;
     // console.log("Clicked id by params", id);
 
+    const data = await allTilesData();
+    // console.log(data);
+
+    const findData = data.find(item => item.id === id);
+    console.log(findData);
+    
+    const {title, description, dimensions,price,image,currency, material,category} = findData;
+
     return (
         <div className='container mx-auto px-10 py-15'>
-            <div className=" card lg:card-side bg-base-100 shadow-sm border ">
-                <figure>
-
-                </figure>
-                <div className="card-body">
-                    <h2 className="card-title">New album is released!</h2>
-                    <p>Click the button to listen on Spotiwhy app.</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Listen</button>
-                    </div>
+            <div className="card lg:card-side shadow-sm border grid grid-cols-1 md:grid-cols-2">
+                <div className='card-body col-span-1 bg-amber-200'>
+                    Image
+                </div>
+                
+                <div className="card-body col-span-1 bg-green-200">
+                    <h2 className="card-title">{title}</h2>
+                    <p>{description}</p>
+                    <p>{dimensions}</p>
+                    <p>{price}</p>
+                    <p>{material}</p>
+                    <p>{category}</p>
+                    <p>{description}</p>
                 </div>
             </div>
        </div>
--- src/lib/fetch_data.js
+++ src/lib/fetch_data.js
@@ -1,7 +1,14 @@
+import path from "path";
+import fs from "fs";
+
 export const allTilesData = async () => {
-    const res = await fetch('/allTilesData/allTiles.json');
-    const tilesData = await res.json();
-    return tilesData;
+    // const res = await fetch('/allTilesData/allTiles.json');
+    // const tilesData = await res.json();
+    // return tilesData;
+
+    const filePath = path.join(process.cwd(), "public", "allTiles.json");
+    const fileContents = fs.readFileSync(filePath, "utf8");
+    const data = JSON.parse(fileContents); return data;
 }
 
 // export const tileDetailsData = async () => {
