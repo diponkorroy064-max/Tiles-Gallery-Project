9dcbf228357650202a23018b5da310bf944fa8f9
diponkorroy064-max <diponkorroy064@gmail.com>
2026-05-05 11:13:09 +0600

    design the login and logout page with daisy UI and funtioning by react-hook-form

--- src/app/(auth)/signIn/page.jsx
+++ src/app/(auth)/signIn/page.jsx
@@ -1,21 +1,32 @@
+'use client'
+import Link from 'next/link';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 
 const SignInPage = () => {
+    const { register, formState: { errors } } = useForm();
+
+
     return (
         <div className='flex flex-col justify-center items-center space-y-3.5 py-15'>
 
             <h2 className='text-3xl font-bold'>Login Your Account</h2>
 
             <form className=''>
-                <fieldset className="fieldset bg-base-200 border-gray-300 border rounded-md w-xs p-4">
+                <fieldset className="fieldset bg-base-200 border-gray-300 border rounded-md w-96 px-4 py-8">
                     <label className="text-[14px] font-semibold">Email</label>
-                    <input type="email" className="input" placeholder="Enter Your Email" />
+                    <input type="email" className="input w-full" placeholder="Enter Your Email" {...register("email", { required: true })} />
+                    {errors.email && <span>Email is required</span>}
 
                     <label className="text-[14px] font-semibold">Password</label>
-                    <input type="password" className="input" placeholder="Enter Your Password" />
+                    <input type="password" className="input w-full" placeholder="Enter Your Password" {...register("email", { required: true})}/>
 
                     <button className="btn btn-neutral mt-4">Login</button>
+
+                    <p className='text-sm text-center mt-4'>
+                        Don`t Have an Account? <Link href='/signUp' className='text-red-500 hover:underline'>Sign Up</Link>
+                    </p>
                 </fieldset>
            </form>
         </div>
@@ -23,3 +34,4 @@ const SignInPage = () => {
 };
 
 export default SignInPage;
+
--- src/app/(auth)/signUp/page.jsx
+++ src/app/(auth)/signUp/page.jsx
@@ -1,11 +1,40 @@
+'use client'
 import React from 'react';
+import { useForm } from 'react-hook-form';
+
 
 const SignUpPage = () => {
+    const { register, formState: { errors } } = useForm();
+
+
     return (
-        <div>
-            This is a signUp Page
+        <div className='container mx-auto flex flex-col justify-center items-center space-y-3.5 py-15'>
+
+            <h2 className='text-3xl font-bold'>Register Your Account</h2>
+
+            <form className=''>
+                <fieldset className="fieldset bg-base-200 border-gray-300 border rounded-md w-99 px-4 py-6">
+                    <label className="text-[14px] font-semibold">Name</label>
+                    <input type="name" className="input w-full" placeholder="Enter Your Name" {...register("name", { required: true })} />
+                    {errors.name && <span>Name is required</span>}
+
+                    <label className="text-[14px] font-semibold">Email</label>
+                    <input type="email" className="input w-full" placeholder="Enter Your Email" {...register("email", { required: true })} />
+                    {errors.email && <span>Email is required</span>}
+
+                    <label className="text-[14px] font-semibold">Photo URL</label>
+                    <input type="text" className="input w-full" placeholder="Enter Your Photo Url" {...register("text", { required: true })} />
+                    {errors.email && <span>Email is required</span>}
+
+                    <label className="text-[14px] font-semibold">Password</label>
+                    <input type="password" className="input w-full" placeholder="Enter Your Password" {...register("password", { required: true})}/>
+
+                    <button className="btn btn-neutral mt-4">Login</button>
+                </fieldset>
+           </form>
         </div>
     );
 };
 
 export default SignUpPage;
+
